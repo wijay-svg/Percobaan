87fb13ee696ca1aa13d565805a63bfa5f6852213
wijay-svg <71633317+wijay-svg@users.noreply.github.com>
2020-11-17 09:55:50 +0700

    Create Coba2.java

---
+++ Coba2/src/coba2/Coba2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coba2;
+
+/**
+ *
+ * @author AXZ
+ */
+public class Coba2 {
+
+    public static void main(String[] args) {
+        System.out.print("Coba1");
+    }
+    
+}
